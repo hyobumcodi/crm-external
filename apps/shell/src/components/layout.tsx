983388667769e0118fb79f2cbef35a8f11a09f1f
hyobumcodi <hb.lee@bodycodi.com>
2025-06-17 12:21:54 +0900

    feat: Shell 라우터 시스템 구현 커스텀 이벤트 기반 앱 간 네비게이션 시스템 추가: Shell 네비게이션 훅 구현

--- apps/shell/src/components/layout.tsx
+++ apps/shell/src/components/layout.tsx
@@ -1,29 +1,14 @@
 import React from "react";
 import { Link, NavLink, Outlet } from "react-router-dom";
-// import { Button } from "@bodycodi/ui-kit";
+import { useShellNavigateListener } from "@bodycodi/shell-router";
+
 import {
-  appLockerRealtimeBoardBasename,
   appScheduleCheckinBasename,
+  appLockerRealtimeBoardBasename,
 } from "../constants/prefix";
 
 const Layout = () => {
-  //   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
-
-  // const handleLogin = async () => {
-  //   await loginWithRedirect({
-  //     appState: {
-  //       returnTo: "/",
-  //     },
-  //   });
-  // };
-
-  // const handleLogout = async () => {
-  //   await logout({
-  //     logoutParams: {
-  //       returnTo: window.location.origin,
-  //     },
-  //   });
-  // };
+  useShellNavigateListener();
 
   return (
     <div>
@@ -32,16 +17,6 @@ const Layout = () => {
           <Link className="global-nav-logo" to="/">
             <span>Bodycodi External</span>
           </Link>
-          {/* {!isAuthenticated && ( */}
-          {/* <div style={{ marginLeft: 20 }}>
-            <Button onClick={handleLogin}>Login</Button>
-          </div> */}
-          {/* )} */}
-          {/* {isAuthenticated && ( */}
-          {/* <div style={{ marginLeft: 20 }}>
-            <Button onClick={handleLogout}>Logout</Button>
-          </div> */}
-          {/* )} */}
           <nav className="global-nav-nav">
             <ul className="global-nav-items">
               <li className="global-nav-item">
@@ -49,7 +24,6 @@ const Layout = () => {
                   className="global-nav-link"
                   to={`${appLockerRealtimeBoardBasename}`}
                 >
-                  {/* <Icon.LockerRealtimeBoard /> */}
                   <span className="global-nav-link-text">락커 현황판</span>
                 </NavLink>
               </li>
@@ -58,20 +32,11 @@ const Layout = () => {
                   className="global-nav-link"
                   to={`${appScheduleCheckinBasename}`}
                 >
-                  {/* <Icon.ScheduleCheckin /> */}
                   <span className="global-nav-link-text">
                     그룹수업 얼굴인식 체크인
                   </span>
                 </NavLink>
               </li>
-              {/* <li className="global-nav-item">
-                <NavLink className="global-nav-link" to={`${appJobBasename}`}>
-                  <Icon.PlaceCheckin />
-                  <span className="global-nav-link-text">
-                    시설 얼굴인식 체크인
-                  </span>
-                </NavLink>
-              </li> */}
             </ul>
           </nav>
         </div>
@@ -79,7 +44,6 @@ const Layout = () => {
       <div className="global-container">
         <Outlet />
       </div>
-      {/* <div className="global-container">{isAuthenticated && <Outlet />}</div> */}
     </div>
   );
 };
--- packages/shell-router/src/index.ts
+++ packages/shell-router/src/index.ts
@@ -1,7 +1,15 @@
-import useShellEvent from "./hooks/use-shell-event";
 import { injectFactory } from "./injector";
 import AppRoutingManager from "./components/app-routing-manager";
+import useShellEvent from "./hooks/use-shell-event";
+import useShellNavigate from "./hooks/use-shell-navigate";
+import useShellNavigateListener from "./hooks/use-shell-navigate-listener";
 
-export { useShellEvent, injectFactory, AppRoutingManager };
+export {
+  injectFactory,
+  useShellEvent,
+  useShellNavigate,
+  useShellNavigateListener,
+  AppRoutingManager,
+};
 
 export type * from "./types";
